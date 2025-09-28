ce28cf5566291e493bcdddc167f610281c975e58
Jerry Whoami <jerry.whoami@gmail.com>
2025-09-27 20:10:48 -0500

    First version of part 2 model_pipeline added

---
+++ model_pipeline/config.py
@@ -0,0 +1,5 @@
+SEED = 7
+DATA_PATH = "./model_pipeline/data/food_delivery_data.csv"
+TARGET = "Delivery_Time_min"
+MODEL_PATH = "./model_pipeline/artifacts/model.pkl"
+TEST_SIZE = 0.2
---
+++ model_pipeline/pipeline.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from xgboost import XGBRegressor
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
+from .config import *
+
+
+NUM_COLS = ["Distance_km", "Preparation_Time_min", "Courier_Experience_yrs"]
+CAT_ONEHOT_COLS = ["Weather", "Time_of_Day", "Vehicle_Type"]
+CAT_ORDINAL_COLS = ["Traffic_Level"]
+
+
+def build_preprocessor():
+    num = Pipeline([
+        ("imputer", SimpleImputer(strategy="median"))
+    ])
+
+    cat_onehot = Pipeline([
+        ("imputer", SimpleImputer(strategy="constant", fill_value="Unknown")),
+        ("ohe", OneHotEncoder(handle_unknown="ignore", sparse_output=False))
+    ])
+
+    cat_ordinal = Pipeline([
+        ("imputer", SimpleImputer(strategy="constant", fill_value="Low")),
+        ("ord", OrdinalEncoder(categories=[["Low","Medium","High"]]))
+    ])
+
+    pre = ColumnTransformer(
+        transformers=[
+            ("num", num, NUM_COLS),
+            ("cat_oh", cat_onehot, CAT_ONEHOT_COLS),
+            ("cat_ord", cat_ordinal, CAT_ORDINAL_COLS),
+        ],
+        remainder="drop"
+    )
+
+    return pre
+
+
+def train():
+    df = pd.read_csv(DATA_PATH)
+
+    y = df[TARGET].values
+    X = df.drop(columns=[TARGET])
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=TEST_SIZE, random_state=SEED)
+
+    pre = build_preprocessor()
+
+    model = XGBRegressor(
+        objective="reg:squarederror",
+        n_estimators=300,
+        learning_rate=0.05,
+        max_depth=6,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        random_state=SEED,
+        n_jobs=-1
+    )
+
+    pipe = Pipeline([
+        ("pre", pre),
+        ("model", model)
+    ])
+
+    pipe.fit(X_train, y_train)
+    
+    joblib.dump(pipe, MODEL_PATH)
+    return MODEL_PATH
+
+
+def evaluate():
+    df = pd.read_csv(DATA_PATH)
+    y = df[TARGET].values
+    X = df.drop(columns=[TARGET])
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=TEST_SIZE, random_state=SEED)
+
+    pipe = joblib.load(MODEL_PATH)
+    y_pred = pipe.predict(X_val)
+
+    mae = mean_absolute_error(y_val, y_pred)
+    rmse = root_mean_squared_error(y_val, y_pred)
+    r2 = r2_score(y_val, y_pred)
+
+    print(f"MAE : {mae:.2f} min")
+    print(f"RMSE: {rmse:.2f} min")
+    print(f"R²  : {r2:.3f}")
+
+if __name__ == "__main__":
+    path = train()
+    print(f"Saved model to {path}")
+
+    evaluate()
